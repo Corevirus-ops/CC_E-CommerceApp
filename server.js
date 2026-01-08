b3e36c2ac428825c90ea73a1c61e1816627b34a9
D0ts_fav_dev <Corevirus1738@gmail.com>
2026-01-08 00:23:28 -0800

    added package details, added use of bodyParser for later use

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const PORT = 3000;
 
-
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`Listening On Port: ${PORT}`)
