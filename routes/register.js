f61c7e1c6015a780623ebe778fcf4103192912c8
D0ts_fav_dev <Corevirus1738@gmail.com>
2026-01-08 20:52:51 -0800

    cleaned up and removed dupe code for cleaner layout and consistency

--- routes/register.js
+++ routes/register.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/index');
 const bcrypt = require('bcrypt');
-
 const checkValidCharacters = require('./validateChar');
 
 const checkUserExists = async (req, res, next) => {
+try {
     const {name, email, password} = req.body;
     const data = await db.query(`SELECT * FROM users WHERE user_email = $1`, [email]);
     if (data.rowCount >= 1) {
@@ -16,31 +16,40 @@ const checkUserExists = async (req, res, next) => {
     req.pass = password;
         next();
     }
+} catch (e) {
+        console.log(e)
+        res.status(500).send();
+    }
 };
 
 //create encrypted password with salt
 const cryptPass = async (pass) => {
-const salt = await bcrypt.genSalt(10);
-const newPass = await bcrypt.hash(pass, salt);
-if (newPass) {
-    return newPass
-} else return;
+try {
+    const salt = await bcrypt.genSalt(10);
+    const newPass = await bcrypt.hash(pass, salt);
+    if (newPass) return newPass
+} catch (e) {
+        console.log(e);
+    }
+    return;
 }
 
 router.post('/', checkValidCharacters, checkUserExists, async (req, res) => {
-const newPass = await cryptPass(req.pass);
-if (newPass) {
-    const user = await db.query(`INSERT INTO users (user_name, user_email) VALUES ($1, $2)`, [req.name, req.email]);
-    const userInfo = await db.query(`SELECT * FROM users WHERE user_email = $1`, [req.email]);
-    const pass = await db.query(`INSERT INTO pass VALUES ($1, $2)`, [userInfo.rows[0].user_id, newPass]); 
-    if (user && pass) {
-    res.status(201).send(user);
-} else {
+    try {
+        const newPass = await cryptPass(req.pass);
+            if (newPass) {
+                const user = await db.query(`INSERT INTO users (user_name, user_email) VALUES ($1, $2)`, [req.name, req.email]);
+                const userInfo = await db.query(`SELECT * FROM users WHERE user_email = $1`, [req.email]);
+                const pass = await db.query(`INSERT INTO pass VALUES ($1, $2)`, [userInfo.rows[0].user_id, newPass]); 
+            if (user && pass) {
+            res.status(201).send(user);
+            return;
+            } 
+                } 
+    } catch (e) {
+        console.log(e);
+    }
     res.status(500).send();
-}
-} else {
-    res.status(500).send();
-}
 });
 
 module.exports = router;
