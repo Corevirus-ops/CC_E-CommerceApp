eae449a10775baf74951f124ce7d05050535f69f
D0ts_fav_dev <Corevirus1738@gmail.com>
2026-01-09 00:00:45 -0800

    added the ability to get all carts

--- routes/cart.js
+++ routes/cart.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const db = require('../db/index');
 const checkValidCharacters = require('./validateChar');
 
+const getUser = async (id) => {
+    const user = await db.query(`SELECT * FROM users WHERE user_id = $1`, [id]);
+    return user;
+}
+
 router.post('/', checkValidCharacters, async (req, res) => {
 try {
     const {id} = req.body;
@@ -10,7 +15,7 @@ try {
         res.status(401).send()
         return
     }
-    const user = await db.query(`SELECT * FROM users WHERE user_id = $1`, [id]);
+    const user = await getUser(id);
     if (user.rowCount) {
         const newCart = await db.query(`INSERT INTO users_cart (user_id) VALUES ($1)`, [id]);
         if (newCart?.rowCount) {
@@ -26,7 +31,25 @@ res.status(500).send();
 });
 
 router.get('/', async (req, res) => {
+    try {
+    const {id} = req.query;
+    if (!id) {
+        res.status(401).send()
+        return
+    }
+    const user = await getUser(id);
+    if (user.rowCount) {
+        const carts = await db.query(`SELECT users_cart.*, users.user_name FROM users_cart, users WHERE users_cart.user_id = users.user_id AND users.user_id = $1`, [id]);
+        if (carts.rowCount) {
+            res.send(carts.rows);
+            return
+        }
+    }
 
+    } catch (e) {
+        console.log(e)
+    }
+    res.status(500).send();
 });
 
 module.exports = router;
