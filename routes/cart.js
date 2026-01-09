1c32172eb06553f55ba70c09179833399288b2ef
D0ts_fav_dev <Corevirus1738@gmail.com>
2026-01-08 23:44:01 -0800

    added ability to create a cart

---
+++ routes/cart.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/index');
+
+router.post('/', async (req, res) => {
+try {
+    const {id} = req.body;
+    if (!id) {
+        res.status(401).send()
+        return
+    }
+    const user = await db.query(`SELECT * FROM users WHERE user_id = $1`, [id]);
+    if (user.rowCount) {
+        const newCart = await db.query(`INSERT INTO users_cart (user_id) VALUES ($1)`, [id]);
+        if (newCart?.rowCount) {
+            res.status(201).send(newCart.rows);
+            return;
+        }
+    }
+
+} catch (e) {
+    console.log(e);
+}
+res.status(500).send();
+});
+
+module.exports = router;
