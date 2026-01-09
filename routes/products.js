fa5da2ce21bca9c58b109f04d2851f31099b009f
D0ts_fav_dev <Corevirus1738@gmail.com>
2026-01-08 22:25:20 -0800

    updated for use with categories, query checks for category, if not exist use blanket search or search by id, else use normal search that has matching category

--- routes/products.js
+++ routes/products.js
@@ -4,8 +4,14 @@ const db = require('../db/index');
 
 router.get('/', async (req, res) => {
     try {
-       const products = await db.query(`SELECT * FROM products`); 
-       if (products.rows[0]) {
+        const category = req.query.category;
+        let products;
+        if (!category) {
+            products = await db.query(`SELECT * FROM products`); 
+        } else {
+            products = await db.query(`SELECT products.*, category_name FROM products, category WHERE products.category_id = category.category_id AND category_name = $1`, [category]);
+        }
+       if (products?.rows[0]) {
         res.send(products.rows);
     } else res.status(404).send(`No Products Found`)
     } catch (e) {
@@ -18,13 +24,22 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const {id} = req.params;
-        const product = await db.query(`SELECT * FROM products WHERE product_id = $1`, [id]);
-        product.rows[0] && res.send(product.rows) || res.status(404).send(`No Products Found`);
+        const category = req.query.category;
+        let product;
+        if (!category) {
+            product = await db.query(`SELECT * FROM products WHERE product_id = $1`, [id]);
+        } else if (category) {
+            product = await db.query(`SELECT products.*, category_name FROM products, category WHERE products.category_id = category.category_id AND product_id = $1 AND category_name = $2`, [id, category]);
+        }
+        product?.rows[0] && res.send(product.rows) || res.status(404).send(`No Products Found`);
 
     } catch (e) {
         console.log(e)
         res.status(500).send();
     }
-})
+});
+
+
+
 
 module.exports = router;
