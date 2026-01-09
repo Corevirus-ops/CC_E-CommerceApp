6447b63cd67b9634ec4cbe0d867df3d06c23f5d2
D0ts_fav_dev <Corevirus1738@gmail.com>
2026-01-08 20:30:32 -0800

    added encryption to password and to update and verify information in db before sending back 201

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/index');
+const bcrypt = require('bcrypt');
 
 const checkValidCharacters = (req, res, next) => {
   const allowedCharsRegex = /^[a-zA-Z0-9\s.,!?-@]+$/; 
@@ -21,21 +22,38 @@ const checkValidCharacters = (req, res, next) => {
 };
 
 const checkUserExists = async (req, res, next) => {
-    const {name, email} = req.body;
+    const {name, email, password} = req.body;
     req.name = name; 
     req.email = email;
+    req.pass = password;
     const data = await db.query(`SELECT * FROM users WHERE user_email = $1`, [email]);
     if (data.rowCount >= 1) {
         res.status(302).send('This Email Is Already Registered!');
     } else {
         next();
     }
+};
+
+//create encrypted password with salt
+const cryptPass = async (pass) => {
+const salt = await bcrypt.genSalt(10);
+const newPass = await bcrypt.hash(pass, salt);
+if (newPass) {
+    return newPass
+} else return;
 }
 
 router.post('/', checkValidCharacters, checkUserExists, async (req, res) => {
-const data = await db.query(`INSERT INTO users (user_name, user_email) VALUES ($1, $2)`, [req.name, req.email]);
-if (data) {
-    res.status(201).send(data.rows);
+const newPass = await cryptPass(req.pass);
+if (newPass) {
+    const user = await db.query(`INSERT INTO users (user_name, user_email) VALUES ($1, $2)`, [req.name, req.email]);
+    const userInfo = await db.query(`SELECT * FROM users WHERE user_email = $1`, [req.email]);
+    const pass = await db.query(`INSERT INTO pass VALUES ($1, $2)`, [userInfo.rows[0].user_id, newPass]); 
+    if (user && pass) {
+    res.status(201).send(user);
+} else {
+    res.status(500).send();
+}
 } else {
     res.status(500).send();
 }
