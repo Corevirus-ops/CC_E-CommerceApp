64ba1ae6af21a6c6681576c05a25b284bdad3d37
D0ts_fav_dev <Corevirus1738@gmail.com>
2026-02-12 13:41:52 -0800

    adding basic routing paths for login and register

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,14 @@
 import {Route, Routes} from 'react-router-dom';
+import LoginMain from './components/login/LoginMain';
+import RegisterMain from './components/register/RegisterMain';
 
 function App() {
   return (
     <div className="App">
 <Routes>
 <Route path="/" element={<h1>Home</h1>} />
+<Route path="/login" element={<LoginMain />} />
+<Route path="/register" element={<RegisterMain />} />
 </Routes>
     </div>
   );
